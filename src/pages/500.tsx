e07a7c11e7d5a3958cedafacef5133cf2f8ea7a5
Masum Gökyüz <masumgokyuz@gmail.com>
2023-10-18 17:14:07 +0300

    changes for prisma model, 404, 500, index page

---
+++ src/pages/500.tsx
@@ -0,0 +1,12 @@
+import { type MyPage } from "@/components/layouts/types";
+
+const ErrorPage: MyPage = () => {
+  return (
+    <div>
+      <h1>500</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
+ErrorPage.Layout = "NoLayout";
