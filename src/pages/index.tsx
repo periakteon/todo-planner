e38cc50e849823fd6de05d0c7838b31e654a2272
Masum Gökyüz <masumgokyuz@gmail.com>
2023-10-18 17:28:40 +0300

    ui components' base setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { type MyPage } from "@/components/layouts/types";
+import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/nextjs";
 
 const Home: MyPage = () => {
@@ -7,6 +8,7 @@ const Home: MyPage = () => {
       <div className="mt-16 rounded-lg border-2 border-dashed border-gray-200 p-4">
         <div className="flex w-full flex-col items-center justify-center">
           <SignInButton mode="modal" />
+          <Button>Buton</Button>
         </div>
       </div>
     </div>
